46ba75802ae68791f2675081300b31ea756c368b
Nikita1025 <nekit1025l@gmail.com>
2023-11-15 21:47:29 +0300

    refresh token

--- src/components/images/images.tsx
+++ src/components/images/images.tsx
@@ -5,7 +5,7 @@ import s from './images.module.scss'
 export const Images = () => {
   const [images, setImages] = useState<DataType[]>([])
   const token =
-    'IGQWROQjlyck90SXM1dFJtTDNSamJHOUItczFSVFVfYTJHb2xrdkVUdHVTaW1jVHRkeXh0S010N3NoanowUGhPenBzUERZATURJcm91ZA2pvWVJtdUNEd0QwTFUwRHZAXQ0ctYmlQTGtrcUljM1ZAndWdOU3VLMXVTOXMZD'
+    'IGQWRPczBUclp0U1NmQ0pDSENNV1ZAfX00wcUhtUmlRYVRCTlc1T0tUZAHBYM2N5ZAHh0Rnh4SXFlRi1lZAkVLSFRLVDlGdGNOTS16MlNURDk4VWdsQXB4bE5iS2tMby0wcFlMbjhxYk5IT3ZA4b0RTUVZACSmk4WjhDTmsZD'
 
   useEffect(() => {
     const fetchData = async () => {
